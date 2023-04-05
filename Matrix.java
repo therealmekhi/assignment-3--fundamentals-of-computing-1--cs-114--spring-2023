a517ce0d4406fc6d9b6588702c3f13b3c4355056
therealmekhi <mekhall@hartford.edu>
2023-04-05 15:40:09 -0400

    Assignment 3 Update

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,48 @@
 public class Matrix {
-}
+
+  private int[][] matrix;
+
+  public Matrix(int size) {
+      matrix = new int[size][size];
+      System.out.println("Matrix created with dimensions " + size + "x" + size);
+  }
+
+  private void swap(int x1, int y1, int x2, int y2) {
+      int temp = matrix[x1][y1];
+      matrix[x1][y1] = matrix[x2][y2];
+      matrix[x2][y2] = temp;
+  }
+
+  public void printMatrix() {
+      for (int row = 0; row < matrix.length; row++) {
+          for (int column = 0; column < matrix[0].length; column++) {
+              if (row == column) {
+                  System.out.print(" \033[43m" + matrix[row] [column] + " \033[0m ");
+              } else {
+                  System.out.print(matrix[row][column] + " ");
+              }
+          }
+          System.out.println();
+      }
+  }
+
+  public void populateMatrix() {
+      int value = 1;
+      for (int row = 0; row < matrix.length; row++) {
+          for (int column = 0; column < matrix[0].length; column++) {
+              matrix[row][column] = value;
+              value++;
+          }
+      }
+  }
+
+  public void flipMatrix() {
+    for (int row = 0; row < this.matrix.length; row++) {
+        for (int column = row + 1; column < this.matrix[row].length; column++) {
+            if (row != column && row + column != this.matrix.length - 1) {
+                swap(row, column, column, row);
+              }
+            }
+          }
+        }
+      }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,25 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.print("Enter the size of the matrix: ");
+      int size = scanner.nextInt();
+
+      Matrix matrix = new Matrix(size);
+
+      System.out.println("\nMatrix before populating:");
+      matrix.printMatrix();
+
+      matrix.populateMatrix();
+
+      System.out.println("\nMatrix after populating:");
+      matrix.printMatrix();
+
+      matrix.flipMatrix();
+
+      System.out.println("\nMatrix after flipping:");
+      matrix.printMatrix();
+    }
   }
 }
